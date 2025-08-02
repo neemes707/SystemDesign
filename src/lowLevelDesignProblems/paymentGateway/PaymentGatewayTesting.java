5b667eb73e455b2626432a093ead6545c78a367b
Neemesh <neemeshkumarsingh@gmail.com>
2025-08-02 23:14:22 +0530

    Functional requirements added for payment gateway

--- src/lowLevelDesignProblems/paymentGateway/PaymentGatewayTesting.java
+++ src/lowLevelDesignProblems/paymentGateway/PaymentGatewayTesting.java
@@ -5,7 +5,11 @@ import lowLevelDesignProblems.paymentGateway.factory.PaymentGatewayFactory;
 import lowLevelDesignProblems.paymentGateway.gateway.IPaymentGateway;
 import lowLevelDesignProblems.paymentGateway.model.Payment;
 import lowLevelDesignProblems.paymentGateway.service.PaymentGatewayService;
-
+/** Functional Requirement
+ * Payment gateway should be extensible - in future we could add new type of payment gateway with minimum code changes
+ * All Payment gateway should follow payment methods in a defined fashion or fixed order
+ * If payment fails, out payment gateway should have retry facility
+ * */
 public class PaymentGatewayTesting {
     public static void main(String[] args) {
 
